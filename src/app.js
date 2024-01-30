1e343a0c351bcb8914c7c3b952c9ffd4e43e73cd
Mikhail <mihailsavuskin13@gmail.com>
2024-01-30 19:50:34 +0300

    swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const route = require('./controller/task.controller')
 const bodyParser = require('body-parser');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/task', route);
 app.use((er, req, res, _next) => res.send(er.message))
 module.exports = app;
