def4aed23b5f6b77f5890f5b1a2c4e6c3b053246
Michail-Seglin <michailsavuskin13@gmail.com>
2024-02-05 13:07:30 +0300

    postDOM

--- client/src/page/Main/Main.tsx
+++ client/src/page/Main/Main.tsx
@@ -1,20 +1,38 @@
 import style from './style.module.scss'
+import axios from 'axios'
 import { useState } from 'react'
 
 function Main() {
-    const [data, setData] = useState();
+    const [task, setTask] = useState({ title: '', description: '' });
+    const [data, setData] = useState([]);
 
+    const getData = (e: any) => {
+        setTask({ ...task, [e.target.name]: e.target.value })
+    }
 
+    const sendData = async () => {
+        try {
+            const data = await axios.post('http://localhost:3000/task/', task);
+            console.log(data);
+
+            // setData(list => [...list, data.data]);
+            setTask({ title: '', description: '' })
+            if (!data.data) throw new Error('error')
+
+        } catch (error) {
+            alert(error)
+        }
+    }
     return (
         <div className={style.wrapper}>
 
             <h1>TODO LIST</h1>
-            <div className={style.header}>
+            <form className={style.header} >
 
-                <input type='text' placeholder='enter note...' name='title'></input>
-                <input type='text' placeholder='enter description...' name='description'></input>
-                <div><button>Create</button></div>
-            </div>
+                <input type='text' placeholder='enter note...' name='title' onChange={getData} value={task.title}></input>
+                <input type='text' placeholder='enter description...' name='description' onChange={getData} value={task.description}></input>
+                <button type='submit' onClick={sendData}>Create</button>
+            </form>
         </div >
     )
 }
--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const route = require('./controller/task.controller')
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const route = require('./controller/task.controller')
+
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/task', route);
