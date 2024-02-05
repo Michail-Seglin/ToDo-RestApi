0e9ce4ee81fc235de4729d69a2ad0ba501cb6304
Michail-Seglin <michailsavuskin13@gmail.com>
2024-02-05 11:29:23 +0300

    styleNew

--- client/src/page/Main/Main.tsx
+++ client/src/page/Main/Main.tsx
@@ -1,6 +1,9 @@
 import style from './style.module.scss'
+import { useState } from 'react'
 
 function Main() {
+    const [data, setData] = useState();
+
 
     return (
         <div className={style.wrapper}>
@@ -8,8 +11,8 @@ function Main() {
             <h1>TODO LIST</h1>
             <div className={style.header}>
 
-                <input type='text' placeholder='enter note...'></input>
-                <input type='text' placeholder='enter description...'></input>
+                <input type='text' placeholder='enter note...' name='title'></input>
+                <input type='text' placeholder='enter description...' name='description'></input>
                 <div><button>Create</button></div>
             </div>
         </div >
